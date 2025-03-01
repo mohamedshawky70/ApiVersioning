031ed2fe78c8ae4d3bd733fef5278caf818ed909
Mohmed Shawky <57459035+mohamedshawky70@users.noreply.github.com>
2025-03-01 16:42:03 +0200

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,21 @@
+//Add api versioning
+services.AddApiVersioning(options =>
+{
+	options.DefaultApiVersion = new ApiVersion(1); //default
+	options.AssumeDefaultVersionWhenUnspecified = true; //لو مجلكش فيرجن اعتمد الديفولن
+	options.ReportApiVersions = true; // اعرضله كل الفيرجنز في الهيدر
+	options.ApiVersionReader = new HeaderApiVersionReader("api-version");// the key in header
+}).AddApiExplorer(options =>
+  {
+	options.GroupNameFormat = "'v'V";
+	options.SubstituteApiVersionInUrl = true;
+  });
+
+//On Controller
+	[ApiVersion(1,Deprecated = true)]
+	[ApiVersion(2)]
+
+//On differenet action
+	[ApiVersion(1)]
+ //--
+ 	[ApiVersion(2)]
